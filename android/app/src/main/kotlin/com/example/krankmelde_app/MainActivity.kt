6a0b291abadc8a4e976c9aed9cb735aee5fa2f6c
livekillu4 <daniel.apenkin08@gmail.com>
2025-12-11 23:08:53 +0100

    deploy flutter web to github pages

--- android/app/src/main/kotlin/com/example/krankmelde_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.krankmelde_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/Pages/PagesInterface.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
-
-abstract class PagesInterface {
-  Widget getButtonText();
-  Widget getWidget();
-  void getFunction(BuildContext context);
-  NavigationDestination getNavigationDestination();
-}
